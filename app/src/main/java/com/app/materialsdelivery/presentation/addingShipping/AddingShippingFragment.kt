89a714d504322c2099bfa9164e243cc4f24ed994
bikti <biktimirov_2003@bk.ru>
2022-09-25 01:42:49 +0500

    сделал экраны(теперь все)

--- app/src/main/java/com/app/materialsdelivery/presentation/addingShipping/AddingShippingFragment.kt
+++ app/src/main/java/com/app/materialsdelivery/presentation/addingShipping/AddingShippingFragment.kt
@@ -5,18 +5,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.app.materialsdelivery.R
+import androidx.lifecycle.ViewModelProvider
+import com.app.materialsdelivery.databinding.FragmentAddingShippingBinding
 
 class AddingShippingFragment : Fragment() {
 
 
     private lateinit var viewModel: AddingShippingViewModel
+    private var _binding: FragmentAddingShippingBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_adding_shipping, container, false)
+    ): View {
+        _binding = FragmentAddingShippingBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[AddingShippingViewModel::class.java]
+
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 
